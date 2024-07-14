43b1fc8ad439612a0ade78f91eaa8d93f1d1b61b
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2024-07-13 19:30:05 -0500

    Extracted urlFor into utils

---
+++ src/utils/utils.ts
@@ -0,0 +1,33 @@
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+const projectId = import.meta.env.PUBLIC_SANITY_PROJECT_ID;
+const dataset = import.meta.env.PUBLIC_SANITY_DATASET;
+const builder = imageUrlBuilder({
+  projectId: projectId,
+  dataset: dataset,
+});
+
+export function urlFor(source: SanityImageSource): string {
+  let url = "";
+  if (typeof source === "string") {
+    url = builder.image(source).url();
+  } else if ("asset" in source) {
+    if ("_ref" in source.asset) {
+      url = builder.image(source.asset._ref).url();
+    } else if ("url" in source.asset) {
+      url = source.asset.url;
+    }
+  } else if (
+    "image" in source &&
+    "asset" in source.image &&
+    "_ref" in source.image.asset
+  ) {
+    url = builder.image(source.image.asset._ref).url();
+  } else {
+    url = "https://picsum.photos/500/500";
+  }
+
+  console.log("Image URL:", url);
+  return url;
+}
