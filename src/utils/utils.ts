2814e126442074f30366703b4a223a42e1a6ced4
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2026-04-05 17:49:02 -0500

    Revert "Updated packages"
    This reverts commit 5a64d216620bcf0e151e2e99e6cd772c7d1e8f4b.

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,8 @@
-import { createImageUrlBuilder } from "@sanity/image-url";
-import type { SanityImageSource } from "@sanity/image-url";
+import imageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { sanityClient } from "sanity:client";
 
-const builder = createImageUrlBuilder(sanityClient);
+const builder = imageUrlBuilder(sanityClient);
 
 export function urlFor(source: SanityImageSource) {
   return builder.image(source);
