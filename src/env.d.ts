de80cc9cc2691afb9db55e47ff4866361283506a
Zane Bartlett <85081861+ZaneBartlett1@users.noreply.github.com>
2026-04-05 20:11:59 -0500

    Revert "fixing node problems"
    This reverts commit b926c49283d9f246118b8f52175e91bd2bd0cbd2.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
-
-declare module "sanity:client" {
-  export const sanityClient: import("@sanity/client").SanityClient;
-}
-
-declare module "sanity:studio" {
-  export const config: import("sanity").Config;
-}
+/// <reference types="@sanity/astro/module" />
